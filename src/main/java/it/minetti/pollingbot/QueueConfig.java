e9198ca037924686842bd0984a2f2347ed1c45f6
Alberto Minetti <alberto.minetti@gmail.com>
2020-11-14 11:47:08 +0100

    same executors as the JVM processors; more logs

--- src/main/java/it/minetti/pollingbot/QueueConfig.java
+++ src/main/java/it/minetti/pollingbot/QueueConfig.java
@@ -1,19 +1,31 @@
 package it.minetti.pollingbot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import static java.lang.Math.max;
+
+@Slf4j
 @Configuration
 @ConditionalOnBean(PollingBot.class)
 public class QueueConfig {
-    public static final int POOL_SIZE = 20;
+    public int poolSize = 1;
+
+    @PostConstruct
+    public void setUp() {
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        poolSize = max(availableProcessors - 1, 1);
+        log.info("Pool size is {}", poolSize);
+    }
 
     @Bean
     public BlockingQueue<Update> updatesQueue() {
@@ -23,10 +35,10 @@ public class QueueConfig {
     @Bean
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(POOL_SIZE);
-        executor.setMaxPoolSize(POOL_SIZE);
+        executor.setCorePoolSize(poolSize);
+        executor.setMaxPoolSize(poolSize);
         executor.setQueueCapacity(1000);
-        executor.setThreadNamePrefix("MyExecutor-");
+        executor.setThreadNamePrefix("thread-");
         executor.initialize();
         return executor;
     }
--- src/main/java/it/minetti/pollingbot/UpdatesScheduler.java
+++ src/main/java/it/minetti/pollingbot/UpdatesScheduler.java
@@ -2,6 +2,7 @@ package it.minetti.pollingbot;
 
 import it.minetti.feature.entrypoint.FeaturesEntryPoint;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.scheduling.annotation.Async;
@@ -11,6 +12,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import javax.annotation.PreDestroy;
 import java.util.concurrent.BlockingQueue;
 
+@Slf4j
 @Component
 @ConditionalOnBean(PollingBot.class)
 public class UpdatesScheduler {
@@ -27,6 +29,7 @@ public class UpdatesScheduler {
     public void scheduleUpdate() {
         while (enabled) {
             Update update = updatesQueue.take();
+            log.debug("Message removed from the queue");
             entryPoint.process(update);
         }
     }
